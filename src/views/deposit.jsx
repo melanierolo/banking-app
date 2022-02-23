e8207f5cbd8cc82ab051801ecd6d3243c70254fa
Melanie Rodas <mrodasl@uni.pe>
2022-02-22 23:43:56 -0500

    added deposit without logic about the login

--- src/views/deposit.jsx
+++ src/views/deposit.jsx
@@ -1,9 +1,12 @@
 import "../App.css";
 import { Container, Card, Row, Form } from "react-bootstrap";
 import Balance from "./Balance";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { UserContext } from "../context/context";
 
 function Deposit() {
+  const ctx = useContext(UserContext);
+  const [isLoggedInDeposit, setIsLoggedInDeposit] = useState(false);
   const [totalState, setTotalState] = useState(0);
   let transactionState = 0; // state of this transaction
   let status = `Account Balance $ ${totalState}`;
@@ -18,26 +21,37 @@ function Deposit() {
 
   return (
     <Container className="min-vh-100 p-4">
-      <Row className="m-auto">
+      <Row className="m-auto my-4">
         <Card style={{ width: "30rem" }} className="row">
-          <Card.Body>
-            <Card.Title>
-              <h1>Deposit</h1>
-            </Card.Title>
-            <Card.Text>
-              Please specify deposit amount
-              <Form onSubmit={handleSubmit}>
-                <Form.Group
-                  className="mb-3"
-                  controlId="formBasicName"
-                ></Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicName">
-                  <Balance onChange={handleChange} />
-                </Form.Group>
-                <h2 id="total">{status}</h2>
-              </Form>
-            </Card.Text>
-          </Card.Body>
+          {isLoggedInDeposit ? (
+            <Card.Body>
+              <Card.Title>
+                <h1>Deposit</h1>
+              </Card.Title>
+              <Card.Text>
+                Please specify deposit amount
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="formBasicName"
+                  ></Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicName">
+                    <Balance onChange={handleChange} />
+                  </Form.Group>
+                  <h2 id="total">{status}</h2>
+                </Form>
+              </Card.Text>
+            </Card.Body>
+          ) : (
+            <Card.Body>
+              <Card.Title className="my-4">
+                <h1>Login required</h1>
+              </Card.Title>
+              <Card.Text>
+                You can't see data for this form.Please login first
+              </Card.Text>
+            </Card.Body>
+          )}
         </Card>
       </Row>
     </Container>
