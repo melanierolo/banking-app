326c6a7f772cac5d48df231c21ed8c71567175e3
Melanie Rodas <mrodasl@uni.pe>
2023-11-10 12:35:11 -0500

    Improved messages Errors

--- src/views/createaccount.jsx
+++ src/views/createaccount.jsx
@@ -1,36 +1,44 @@
-import "../App.css";
-import React from "react";
-import { Card, Row, Container, Form, Button } from "react-bootstrap";
-import { Fragment, useState } from "react";
-import { useContext } from "react";
-import { UserContext } from "../context/context";
+import '../App.css';
+import React, { useState } from 'react';
+import { Card, Row, Container, Form, Button } from 'react-bootstrap';
+import { useContext } from 'react';
+import { UserContext } from '../context/context';
 
 function CreateAccount() {
-  const [status, setStatus] = useState("");
+  const [status, setStatus] = useState('');
   const [show, setShow] = useState(true);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const ctx = useContext(UserContext);
+
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
   const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-  function validate(field, label) {
+  function validate(field, label, setError) {
     if (!field) {
-      setStatus("Error: " + label);
-      setTimeout(() => setStatus(""), 5000);
+      setError(true);
+      setStatus('Error: ' + label);
+      setTimeout(() => {
+        setStatus('');
+        setError(false);
+      }, 5000);
       return false;
     }
     return true;
   }
 
   function handleCreate() {
-    console.log(name, email, password);
-    if (!validate(name, "name")) return;
-    if (!validate(email, "email")) return;
-    if (!validate(password, "password")) return;
+    if (!validate(name, 'name', setNameError)) return;
+    if (!validate(email, 'email', setEmailError)) return;
+    if (!validate(password, 'password', setPasswordError)) return;
+
     ctx.users.push({
       balance: 100,
       name,
@@ -39,93 +47,100 @@ function CreateAccount() {
       isLoggedIn: false,
       id: ctx.users.length + 1,
     });
+
     setShow(false);
   }
 
   function clearForm() {
-    setName("");
-    setEmail("");
-    setPassword("");
+    setName('');
+    setEmail('');
+    setPassword('');
     setShow(true);
   }
 
   return (
     <>
-      <Container className="p-4 container d-flex justify-content center align-items-center min-vh-100">
-        <Row className="m-auto">
-          <Card style={{ width: "30rem" }} className="row py-4" status={status}>
+      <Container className='p-4 container d-flex justify-content center align-items-center min-vh-100'>
+        <Row className='m-auto'>
+          <Card style={{ width: '30rem' }} className='row py-4' status={status}>
             {show ? (
               <Form>
-                <Card.Title className="text-center w-100 p-3">
+                <Card.Title className='text-center w-100 p-3'>
                   <h1>Create Account</h1>
                 </Card.Title>
-                <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Group className='mb-3' controlId='formBasicName'>
                   <Form.Label>Name :</Form.Label>
                   <Form.Control
-                    type="text"
-                    placeholder="Enter name"
+                    type='text'
+                    placeholder='Enter name'
                     value={name}
-                    onChange={(e) => setName(e.currentTarget.value)}
+                    onChange={(e) => {
+                      setName(e.currentTarget.value);
+                      setNameError(false);
+                    }}
                   />
-                  {name === "" && (
-                    <Form.Label className="text-danger">
+                  {nameError && (
+                    <Form.Label className='text-danger'>
                       Name required
                     </Form.Label>
                   )}
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className='mb-3' controlId='formBasicEmail'>
                   <Form.Label>Email address :</Form.Label>
                   <Form.Control
-                    type="email"
-                    placeholder="Enter email"
+                    type='email'
+                    placeholder='Enter email'
                     value={email}
-                    onChange={(e) => setEmail(e.currentTarget.value)}
+                    onChange={(e) => {
+                      setEmail(e.currentTarget.value);
+                      setEmailError(false);
+                    }}
                   />
-                  {emailRegex.test(email) === false && (
-                    <Form.Label className="text-danger">
+                  {emailError && (
+                    <Form.Label className='text-danger'>
                       Email required or invalid
                     </Form.Label>
                   )}
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className='mb-3' controlId='formBasicPassword'>
                   <Form.Label>Password</Form.Label>
                   <Form.Control
-                    type="password"
-                    placeholder="Password"
+                    type='password'
+                    placeholder='Password'
                     value={password}
-                    onChange={(e) => setPassword(e.currentTarget.value)}
+                    onChange={(e) => {
+                      setPassword(e.currentTarget.value);
+                      setPasswordError(false);
+                    }}
                   />
-                  {passwordRegex.test(password) === false && (
-                    <Form.Label className="text-danger">
+                  {passwordError && (
+                    <Form.Label className='text-danger'>
                       Minimum eight characters, at least one letter and one
                       number
                     </Form.Label>
                   )}
                 </Form.Group>
+
                 <Button
-                  className="d-flex justify-content-center mx-auto px-4"
-                  variant="primary"
-                  size="lg"
-                  type="submit"
+                  className='d-flex justify-content-center mx-auto px-4'
+                  variant='primary'
+                  size='lg'
+                  type='submit'
                   onClick={handleCreate}
-                  disabled={
-                    name === "" ||
-                    passwordRegex.test(password) === false ||
-                    emailRegex.test(email) === false
-                  }
+                  disabled={name === '' && email === '' && password === ''}
                 >
                   Sign up
                 </Button>
               </Form>
             ) : (
               <>
-                <Container className="text-center w-100 p-3 ">
+                <Container className='text-center w-100 p-3 '>
                   <h1>You have signed up successfully</h1>
                   <button
-                    type="submit"
-                    className="btn btn-light"
+                    type='submit'
+                    className='btn btn-light'
                     onClick={clearForm}
                   >
                     Add another account
